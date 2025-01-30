04727da1f7490d96160541a33c3ce7266ea8e59b
kz-n <64893077+kz-n@users.noreply.github.com>
2025-01-30 19:25:53 +0100

    Delete .rollup.cache/C/Users/kz-n/WebstormProjects/untitled/dist directory

--- .rollup.cache/C/Users/kz-n/WebstormProjects/untitled/dist/public/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { AudioPlayer } from './components/AudioPlayer';
-export function App() {
-    return (React.createElement("div", null,
-        React.createElement(AudioPlayer, null),
-        React.createElement("h1", null, "Hello from React + Electron!")));
-}
-//# sourceMappingURL=App.js.map
--- .rollup.cache/C/Users/kz-n/WebstormProjects/untitled/dist/public/src/components/AudioPlayer.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import { MusicList } from './MusicList';
-export function AudioPlayer() {
-    const [music, setMusic] = useState('');
-    return React.createElement("div", null,
-        React.createElement("audio", { src: music, controls: true }),
-        React.createElement(MusicList, { music: setMusic }));
-}
-//# sourceMappingURL=AudioPlayer.js.map
